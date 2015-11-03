6cfae1f6bf388f94efd07e14dadd37e6233c75a3
markedwardmurray <markedwardmurray@gmail.com>
2015-11-03 16:17:52 -0500

    add solution & tests

--- swift-deli-optionals/Sandbox.swift
+++ swift-deli-optionals/Sandbox.swift
@@ -8,3 +8,49 @@ import Foundation
 
 */
 
+func stringForDeliLine(deliLine: [String]?) -> String? {
+    
+    if var deliLine = deliLine {
+        if deliLine.count == 0 {
+            return nil
+        }
+        
+        var result = "The line is:"
+        for var i = 0; i < deliLine.count; i++ {
+            let numberInLine = i + 1
+            result += "\n\(numberInLine). \(deliLine[i])"
+        }
+        print(result)
+        return result
+    } else {
+        return nil
+    }
+}
+
+func addNameTo(name: String, deliLine: [String]?) -> [String] {
+    if var deliLine = deliLine {
+        deliLine.append(name)
+        print("Welcome \(name)! You are number \(deliLine.count) in line.")
+        return deliLine
+    } else {
+        return [name]
+    }
+}
+
+func serveNextCustomerInDeliLine(deliLine: [String]?) -> [String]? {
+    
+    if var deliLine = deliLine {
+        if deliLine.count > 0 {
+            let nextCustomer = deliLine.removeFirst()
+            print("Now serving \(nextCustomer)")
+            return deliLine
+        } else {
+            print("The line is currently empty.")
+            return nil
+        }
+    } else {
+        print("The line is currently empty.")
+        return nil
+    }
+}
+
--- swift-deli-optionalsTests/SandboxSpec.swift
+++ swift-deli-optionalsTests/SandboxSpec.swift
@@ -6,11 +6,111 @@ import Nimble
 
 class SandboxSpec: QuickSpec {
     override func spec() {
-        describe("Sandbox") {
-            describe("") {
-                it("") {
-                    
-                }
+        var nilArray: [String]? = nil
+        var empty: [String]? = []
+        var precollege: [String]? = []
+        var ios: [String]? = []
+        
+        beforeEach {
+            nilArray = nil
+            empty = []
+            precollege = ["Victoria", "Danny", "Lyel"]
+            ios = ["Joe", "Tim", "Jim", "Tom"]
+        }
+        
+        describe("stringForDeliLine(_:)") {
+            it("should return a string optional containing nil when the argument array is nil") {
+                
+                expect(stringForDeliLine(nilArray)).to(beNil())
+            }
+            
+            it("should return a string optional containing nil when the argument array is empty") {
+                
+                expect(stringForDeliLine(empty)).to(beNil())
+            }
+            
+            it("should return a string describing a line with three names") {
+                let expected = "The line is:\n1. Victoria\n2. Danny\n3. Lyel"
+                
+                expect(stringForDeliLine(precollege)).to(match(expected))
+            }
+            
+            it("should return a string describing a line with four names") {
+                let expected = "The line is:\n1. Joe\n2. Tim\n3. Jim\n4. Tom"
+                
+                expect(stringForDeliLine(ios)).to(match(expected))
+            }
+        }
+        
+        describe("addNameTo(_:deliLine:)") {
+            it("should create a new array with name 'Al' when the deliLine argument is nil") {
+                let result = addNameTo("Al", deliLine: empty)
+                let expected = ["Al"]
+                
+                expect(result).to(equal(expected))
+            }
+            
+            it("should create a new array with name 'Zach' when the deliLine argument is nil") {
+                let result = addNameTo("Zach", deliLine: empty)
+                let expected = ["Zach"]
+                
+                expect(result).to(equal(expected))
+            }
+            
+            it("should add the name 'Al' to an empty array") {
+                let result = addNameTo("Al", deliLine: empty)
+                let expected = ["Al"]
+                
+                expect(result).to(equal(expected))
+            }
+            
+            it("should add the name 'Chris' to an empty array") {
+                let result = addNameTo("Chris", deliLine: empty)
+                let expected = ["Chris"]
+                
+                expect(result).to(equal(expected))
+            }
+            
+            it("should add the name 'Dan' to the end of the 'precollege' array") {
+                let result = addNameTo("Dan", deliLine: precollege)
+                let expected = ["Victoria", "Danny", "Lyel", "Dan"]
+                
+                expect(result).to(equal(expected))
+            }
+            
+            it("should add the name 'Mark' to the end of the 'ios' array") {
+                let result = addNameTo("Mark", deliLine: ios)
+                let expected = ["Joe", "Tim", "Jim", "Tom", "Mark"]
+                
+                expect(result).to(equal(expected))
+            }
+        }
+
+        describe("serveNextCustomerInDeliLine(_:)") {
+            it("should return an array optional containing nil when the argument optional contains nil") {
+                let result = serveNextCustomerInDeliLine(nilArray)
+                
+                expect(result).to(beNil())
+            }
+            
+            it("should return an array optional containing nil when an empty array is submitted") {
+                let result = serveNextCustomerInDeliLine(empty)
+                
+                expect(result).to(beNil())
+            }
+            
+            it("should remove 'Victoria' from the 'precollege' array") {
+                let result = serveNextCustomerInDeliLine(precollege)
+                let expected = ["Danny", "Lyel"]
+                
+                expect(result).to(equal(expected))
+            }
+            
+            it("should remove 'Joe' from the 'ios' array") {
+                let result = serveNextCustomerInDeliLine(ios)
+                let expected = ["Tim", "Jim", "Tom"]
+                
+                expect(result).to(equal(expected))
             }
         }
     }
